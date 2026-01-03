4c3015a3a9aa98a0125da8de59d3ef336391279c
Thi Dinh <vietthidinh2001@gmail.com>
2026-01-03 21:55:11 +0100

    fix: wrong data type for CPU flags

--- lib/cpu.h
+++ lib/cpu.h
@@ -5,6 +5,19 @@
 #include <stdint.h>
 #include "mmu.h"
 
+/// https://gbdev.io/pandocs/CPU_Registers_and_Flags.html
+///
+/// 7     bit     0
+/// ----       ----
+/// Z N H C 0 0 0 0
+/// | | | | | | | |
+/// | | | | | | | |
+/// | | | | | | | |
+/// | | | | +-+-+-+- Always 0
+/// | | | +--------- Carry flag
+/// | | + ---------- Half Carry flag (BCD)
+/// | +------------- Substraction flag (BCD)
+/// +--------------- Zero flag
 typedef enum
 {
     Z_FLAG = 1 << 7,
@@ -17,7 +30,7 @@ typedef struct
 {
     uint16_t sp;
     uint16_t pc;
-    FlagRegister flags;
+    uint8_t flags;
     uint8_t a, b, c, d, e, h, l;
     MMU *mmu;
 } CPU;
