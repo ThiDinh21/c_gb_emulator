5c3bcb9ffd2e5cd7f8b2e026af95491ef83b3b3a
Thi Dinh <vietthidinh2001@gmail.com>
2026-01-09 03:13:03 +0100

    add: opcode switch for 1st line of opcode table

--- src/cpu.c
+++ src/cpu.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "cpu.h"
+#include "opcodes.h"
 #include "errors.h"
 
 CPU *init_cpu(void)
@@ -25,12 +27,85 @@ void run_cpu(CPU *cpu)
     while (1)
     {
         // Fetch opcode
-        // uint8_t opcode = get_opcode(cpu->mmu, cpu->program_counter);
-        cpu->program_counter++;
+        uint8_t opcode = cpu_fetch_u8(cpu);
+        uint8_t cycles = 0;
 
         // Execute opcode
-
-        panic_unimplemented();
+        switch (opcode)
+        {
+        case 0x00:
+            // NOP
+            cycles = op_00();
+            break;
+        case 0x01:
+            // LD BC, u16
+            cycles = op_01(cpu);
+            break;
+        case 0x02:
+            // LD (BC), A
+            cycles = op_02(cpu);
+            break;
+        case 0x03:
+            // INC BC
+            cycles = op_03(cpu);
+            break;
+        case 0x04:
+            // INC B
+            cycles = op_04(cpu);
+            break;
+        case 0x05:
+            // DEC B
+            cycles = op_05(cpu);
+            break;
+        case 0x06:
+            // LD B, u8
+            cycles = op_06(cpu);
+            break;
+        case 0x07:
+            // RLCA
+            cycles = op_07(cpu);
+            break;
+        case 0x08:
+            // LD (u16), SP
+            cycles = op_08(cpu);
+            break;
+        case 0x09:
+            // ADD HL, BC
+            cycles = op_09(cpu);
+            break;
+        case 0x0A:
+            // LD A, (BC)
+            cycles = op_0a(cpu);
+            break;
+        case 0x0B:
+            // DEC BC
+            cycles = op_0b(cpu);
+            break;
+        case 0x0C:
+            // INC C
+            cycles = op_0c(cpu);
+            break;
+        case 0x0D:
+            // DEC C
+            cycles = op_0d(cpu);
+            break;
+        case 0x0E:
+            // LD C, u8
+            cycles = op_0e(cpu);
+            break;
+        case 0x0F:
+            // RRCA
+            cycles = op_0f(cpu);
+            break;
+        // case 0x10 ... 0xFF:
+        //     panic_unimplemented();
+        default:
+            char err_msg[30];
+            snprintf(err_msg, 30, "Opcode not recognized: 0x%x\n", opcode);
+            panic(err_msg, ERR_UNKNOWN_INSTRUCTION);
+        }
+        printf("%d", cycles);
+        return;
     }
 }
 
