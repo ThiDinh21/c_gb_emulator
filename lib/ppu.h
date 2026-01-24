cdb5179cb04dddbd92d3fb4e7b645bf007819a4d
Thi Dinh <vietthidinh2001@gmail.com>
2026-01-24 22:47:17 +0100

    add: fetch background pixel line

--- lib/ppu.h
+++ lib/ppu.h
@@ -7,6 +7,10 @@ void ppu_step(CPU *cpu, uint8_t cycles);
 
 void ppu_render_line(PPU *ppu);
 
+uint8_t tile_index_to_pixel_bg_win(PPU *ppu, uint8_t tile_index, uint8_t lcdc4, uint8_t abs_x, uint8_t abs_y);
+
+uint8_t get_color_from_palette(uint8_t palette_reg, uint8_t color_id);
+
 uint8_t get_mode_3_length(PPU *ppu);
 
 void send_vblank_interrupt(CPU *cpu);
--- src/ppu.c
+++ src/ppu.c
@@ -78,7 +78,78 @@ void ppu_step(CPU *cpu, uint8_t cycles)
 void ppu_render_line(PPU *ppu)
 {
     panic_unimplemented("PPU render");
-    ppu->mode_3_length = 172;
+
+    // Screen is 160 x 144, each line is 32 tiles, each tiles is 8x8 pixels
+    uint8_t line_pixels[160] = {0};
+
+    // FIFO Pixel fetcher (5 steps)
+    // Step 1: Get tile
+    // Always use tilemap 0x9800-0x9BFF or use 0x9C00-0x9FFF when:
+    // - LCDC.3 is enabled and the X coordinate of the current scanline is not inside the window.
+    // - LCDC.6 is enabled and the X coordinate of the current scanline is inside the window.
+    uint8_t lcdc_3 = ppu->lcd_control & (1 << 3) ? 1 : 0;
+    uint8_t lcdc_4 = ppu->lcd_control & (1 << 4) ? 1 : 0; // Addressing mode of tile data
+    uint16_t bg_tile_map = (lcdc_3 ? 0x9C00 : 0x9800) - 0x8000;
+    // Calc tile's row
+    // Add pixels (ly + scy) then divide by 8 to get tile row
+    // & 255 for the vertical wrap-around
+    uint8_t abs_pixel_y = ppu->lcd_y + ppu->bg_y;
+    uint8_t tile_y = abs_pixel_y / 8;
+
+    // Loop through each pixel of a row
+    for (int i = 0; i < 160; i++)
+    {
+        // Calc tile col
+        uint8_t abs_pixel_x = i + ppu->bg_x;
+        uint8_t tile_x = abs_pixel_x / 8; // From 0..31
+        // Calculate Address: base + (row * 32) + col
+        uint16_t tile_index = ppu->vram[bg_tile_map + tile_y * 32 + tile_x];
+        uint8_t pixel_color_id = tile_index_to_pixel_bg_win(ppu, tile_index, lcdc_4, abs_pixel_x, abs_pixel_y);
+        line_pixels[i] = get_color_from_palette(ppu->bg_palette, pixel_color_id);
+    }
+
+    uint8_t lcdc_6 = ppu->lcd_control & (1 << 6) ? 1 : 0;
+    uint16_t window_tile_map = (lcdc_6 ? 0x9C00 : 0x9800) - 0x8000;
+    // uint8_t win_x_start =
+
+    // Get bg tile (9800–9BFF):
+    // bg_x = ;
+
+    // Step 2: Get tile data low
+    // Step 3: Get tile data high
+    // Step 4: Sleep (ignored)
+    // Step 5: Push
+}
+
+uint8_t tile_index_to_pixel_bg_win(PPU *ppu, uint8_t tile_index, uint8_t lcdc4, uint8_t abs_x, uint8_t abs_y)
+{
+    uint8_t byte1, byte2;
+    uint16_t index;
+    uint8_t col = abs_x % 8; // both must in range 0..7 as a tile is 8x8
+    uint8_t row = abs_y % 8;
+
+    // Tile data address mode: https://gbdev.io/pandocs/Tile_Data.html#vram-tile-data
+    if (lcdc4)
+    {
+        index = tile_index * 16 + row * 2;
+    }
+    else
+    {
+        int8_t signed_tile_index = (int8_t)tile_index;
+        index = 0x1000 + signed_tile_index * 16 + row * 2;
+    }
+    byte1 = ppu->vram[index];
+    byte2 = ppu->vram[index + 1];
+
+    uint8_t bit_pos = 7 - col;
+    uint8_t lsb = (byte1 >> bit_pos) & 0b1;
+    uint8_t msb = (byte2 >> bit_pos) & 0b1;
+    return (msb << 1) | lsb;
+}
+
+uint8_t get_color_from_palette(uint8_t palette_reg, uint8_t color_id)
+{
+    return (palette_reg >> (color_id * 2)) & 0b11;
 }
 
 // Get the length of mode 3 before rendering
